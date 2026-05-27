e325e8c2fdfc47c493a611e0ce5b9a91b180115b
Jack Merrigan <61290573+merrigan611@users.noreply.github.com>
2026-05-27 09:34:32 -0700

    Add files via upload

---
+++ roulette/roulette/rouellet.hpp
@@ -0,0 +1,37 @@
+//
+//  rouellet.hpp
+//  
+//
+//  Created by Jack Francis Merrigan on 5/26/26.
+//
+
+#ifndef roulette_hpp
+#define roulette_hpp
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class roulette {
+
+public:
+    roulette();
+    void playGame();
+
+private:
+    std::string track(int a);
+    void results();
+    int spin();
+    int bet(int b);
+
+    int red;
+    int black;
+    int green;
+    int betAmount;
+    int games;
+    int money;
+};
+
+#endif /* roulette_hpp */
---
+++ roulette/roulette/roulette.h/rouellet.cpp
@@ -0,0 +1,83 @@
+//
+//  rouellet.cpp
+//
+//  Created by Jack Francis Merrigan on 5/26/26.
+//
+
+#include "rouellet.hpp"
+
+rouellet(){
+    red= 0;
+    black=0;
+    green=0;
+    money= 50000;
+    games=0;
+    
+}
+void rouellet::playGame(){
+
+    std::string playType;
+    std::cout << "Do you want to bet on a color (Y/N)? ";
+    std::cin >> playType;
+
+    if(playType == "Y"){
+        do{
+            int pot = bet(money);
+            int landedOn = spin();
+            std::string color = track(landedOn);
+
+            if(landedOn % 2 == 1 && color != "green"){
+                money += pot * 2;
+            }
+            else{
+                money -= pot;
+            }
+            games++;
+
+        }while(games < 1001 && money > 0);
+
+        results();
+    }
+    else{
+        std::cout << "Game cancelled." << std::endl;
+    }
+}
+
+int rouellet::bet(int ThePot){
+    ThePot = (rand() % money) + 1;
+
+    if(money > 0){
+        return ThePot;
+    }
+    else{
+        return 1001;
+    }
+}
+
+std::string rouellet::track(int a){
+
+    if(a % 2 == 1){
+        black++;
+        return "black";
+    }
+    else if(a == 0){
+        green++;
+        return "green";
+    }
+    else{
+        red++;
+        return "red";
+    }
+}
+
+void rouellet::results(){
+    std::cout << red   << " came up this many times" << std::endl
+              << black << " came up this many times" << std::endl
+              << green << " came up this many times" << std::endl
+              << money << " :is the amount of money left/won" << std::endl;
+}
+
+int rouellet::spin(){
+    return rand() % 37;
+}
+}
